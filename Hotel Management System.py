dd76cfaea76bd30005e1b337b8a69440a834fa97
Bocz Vlogs <144637954+boczvlogs@users.noreply.github.com>
2023-11-17 12:05:07 +0800

    Update Hotel Management System.py

--- Hotel Management System.py
+++ Hotel Management System.py
@@ -8,7 +8,7 @@ class hotelmanage:
         self.r=r
         self.t=t
         self.p=p 
-        self.s=s 
+        self.s=s  
         self.a=a
         self.name=name   
         self.address=address 
