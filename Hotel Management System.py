f54d916bb65267fde61475b9ab6245604e74f81a
Bocz Vlogs <144637954+boczvlogs@users.noreply.github.com>
2023-11-17 12:05:21 +0800

    Update Hotel Management System.py

--- Hotel Management System.py
+++ Hotel Management System.py
@@ -3,7 +3,6 @@ class hotelmanage:
     def __init__(self,rt='',s=0,p=0,r=0,t=0,a=1000,name='',address='',cindate='',coutdate='',rno=1):
  
         print ("\n\n*****WELCOME TO HOTEl DE Bocz*****\n")
-   
         self.rt=rt 
         self.r=r
         self.t=t
